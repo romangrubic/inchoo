ea4b87de482f50f22cc6b6db378f709e003f37e3
Roman Grubic <grubic.roman@gmail.com>
2022-05-11 17:13:23 +0200

    Adding table Photo with constraints (primary key (id) and foreign key with delete cascade(user_id))

--- Database.sql
+++ Database.sql
@@ -6,11 +6,20 @@ create table User(
     id int not null auto_increment,
     username varchar(255) not null,
     email varchar(255) not null,
-    user_password char(60) not null,
+    password char(60) not null,
     PRIMARY KEY (id),
     UNIQUE (email)
 );
 
+create table Photo(
+    id int not null auto_increment,
+    name varchar(255) not null,
+    address varchar(255),
+    user_id int not null,
+    created_at datetime not null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) references User(id) on delete cascade
+);
 
 
 
