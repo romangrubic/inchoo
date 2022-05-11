49174afb8a5f8d6c10b6b0b0a2cb99b8b7d56523
Roman Grubic <grubic.roman@gmail.com>
2022-05-11 19:06:06 +0200

    Calling the right class and method from $route.

--- app/core/App.php
+++ app/core/App.php
@@ -5,8 +5,35 @@ class App
     public static function start()
     {
         $route = App::getRoute();
-        echo($route);
-        exit;
+        $parts = explode(DIRECTORY_SEPARATOR, $route);
+
+        // This one takes parts[1] and sets the name of the controller class
+        $class='';
+        if (!isset($parts[1]) || $parts[1] === '') {
+            $class = 'Index';
+        } else {
+            $class = ucfirst($parts[1]);
+        }
+        $class .= 'Controller';
+
+        // This one takes parts[2] and sets the name of the method
+        $method = '';
+        if (!isset($parts[2]) || $parts[2] == '') {
+            $method = 'index';
+        } else {
+            $method = lcfirst($parts[2]);
+        }
+
+        // Now we check if the class and method in the same class exist and activate it. Otherwise we show 404error page.
+        if (class_exists($class) && method_exists($class, $method)) {
+            $instance = new $class();
+            $instance->$method();
+        } else {
+            $view = new View();
+            $view->render('error404', [
+                'error' => $class . '->' . $method
+            ]);
+        }
     }
 
     // Getting the user route so that we can use it to point to correct controller and method
